7103a6634879eb550aa75b46529246f26cd5d48d
hossein abdizadeh <79523696+h-abdizadeh@users.noreply.github.com>
2023-12-06 08:38:17 +0330

    2023-12-6_1402-9-15

---
+++ FanavaranBlog/Models/DatabaseContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Security.Permissions;
+
+namespace FanavaranBlog.Models;
+
+public class DatabaseContext:DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        //base.OnConfiguring(options);
+        options.UseSqlite($"data source=BlogDb.db");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        //base.OnModelCreating(modelBuilder);
+    }
+
+    public virtual DbSet<Blog> Blogs { get; set; }
+}
